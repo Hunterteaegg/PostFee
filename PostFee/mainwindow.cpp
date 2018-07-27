f5a1afb1800957b95ab9da4f44383b388b6ca71e
hunterteaegg <1009468031@qq.com>
2018-07-28 01:42:11 +0800

    domestic part building

--- PostFee/mainwindow.cpp
+++ PostFee/mainwindow.cpp
@@ -1,5 +1,8 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTextEdit>
+
+#pragma execution_character_set("utf-8")
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,62 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_button_cal_clicked()
+{
+    QString temp_weight=this->ui->textbox_weight->document()->toPlainText();
+
+    weight=temp_weight.toDouble();
+
+
+    if(status==POSTCARD)
+    {
+        ui->textbox_result->document()->setPlainText("邮费为0.8元 ");
+    }
+    else if(status==MAIL)
+    {
+        if(area==INSIDE)
+        {
+            if(weight<=20)
+            {
+                postfee=0.8;
+            }
+            else if(weight>20&&weight<=100)
+            {
+                postfee=0.8+((int)(((weight-20)/20)+1))*0.8;
+            }
+            else
+            {
+                postfee=8+((int)(((weight-100)+1)/100))*1.2;
+            }
+            QString text;
+            text.append("邮费为");
+            text.append(QString::number(postfee));
+            text.append("元");
+
+            ui->textbox_result->document()->setPlainText(text);
+        }
+    }
+}
+
+void MainWindow::on_button_mail_clicked()
+{
+    status=MAIL;
+}
+
+void MainWindow::on_button_postcard_clicked()
+{
+    status=POSTCARD;
+}
+
+void MainWindow::on_botton_inside_clicked()
+{
+    area=INSIDE;
+}
+
+void MainWindow::on_botton_outside_clicked()
+{
+    area=OUTSIDE;
 }
--- PostFee/mainwindow.h
+++ PostFee/mainwindow.h
@@ -15,8 +15,29 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
+
+private slots:
+    void on_button_cal_clicked();
+
+    void on_button_mail_clicked();
+
+    void on_button_postcard_clicked();
+
+    void on_botton_inside_clicked();
+
+    void on_botton_outside_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    enum Status{MAIL,POSTCARD};
+    Status status;
+    enum Area{INSIDE,OUTSIDE};
+    Area area;
+
+    double weight;
+    double postfee;
 };
 
 #endif // MAINWINDOW_H
